024b5c4df63c0f3aec4fd5195cc100b8a2617fe6
Tomas Lugo <ge52bar@mytum.de>
2020-04-03 16:46:17 -0600

    -Formats step_9

--- Practice6/step_9.s
+++ Practice6/step_9.s
@@ -15,8 +15,8 @@
 @	Data Section
 @ ---------------------------------------
 	
-	.data
-	.balign 4	
+.data
+.balign 4	
 prompt:	.asciz	"> "
 format: .asciz 	"%d"
 num:	.int	0
@@ -26,25 +26,25 @@ output: .asciz 	"your input: %d\n"
 @	Code Section
 @ ---------------------------------------
 	
-	.text
-	.global main
-	.extern printf
-	.extern scanf
+.text
+.global main
+.extern printf
+.extern scanf
 
-main:   push 	{ip, lr}	@ push return address + dummy register
+main:
+push {ip, lr}	@ push return address + dummy register
 				@ for alignment
 
-	ldr	r0, =prompt	@ print the prompt
-	bl	printf
+ldr	r0, =prompt	@ print the prompt
+bl	printf
 
-	ldr     r0, =format	@ call scanf, and pass address of format
-	ldr	r1, =num	@ string and address of num in r0, and r1,
-	bl	scanf		@ respectively.
+ldr r0, =format	@ call scanf, and pass address of format
+ldr	r1, =num	@ string and address of num in r0, and r1,
+bl	scanf		@ respectively.
 
-	ldr	r1, =num	@ print num formatted by output string.
-	ldr	r1, [r1]
-	ldr	r0, =output
-	bl	printf
+ldr	r1, =num	@ print num formatted by output string.
+ldr	r1, [r1]
+ldr	r0, =output
+bl	printf
 
-
-        pop 	{ip, pc}	@ pop return address into pc
+pop	{ip, pc}	@ pop return address into pc
